340a23d3e956990e21612e80b0688377e6abd889
Sahidul Islam <jonyahmed19@gmail.com>
2021-08-14 11:06:00 +0600

    added header components for whole websites

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
-
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 const App = () => {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={Shop} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={Shop} />
+      </Switch>
     </div>
   );
 };
